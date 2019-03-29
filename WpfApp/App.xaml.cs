cc09c70cbcecdad6267437beda5e9b8948f19535
jason.gill <jason.gill@markit.com>
2019-03-29 12:36:54 -0500

    Created a startup event

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WpfApp
 {
@@ -13,5 +7,13 @@ namespace WpfApp
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Start SignalR here
+
+            // Create the startup window
+            MainWindow wnd = new MainWindow {Title = "Wpf App"};
+            wnd.Show();
+        }
     }
 }
